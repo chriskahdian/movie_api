4463e3e61a249542a1470dd12c1a880158ddf795
chriskahdian <34841848+chriskahdian@users.noreply.github.com>
2020-09-21 15:49:47 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const app = express();
 
 app.use(morgan("common"));
 
+app.use(express.static("public"));
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("error");
+});
+
 app.get("/movies", (req, res) => {
   res.json(top10);
 });
 
 app.get("/", (req, res) => {
   res.send("Welcome to my movie list");
-});
-
-app.use(express.static("public"));
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("error");
 });
 
 app.listen(8080, () => console.log("app running on port 8080"));
