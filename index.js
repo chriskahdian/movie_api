1ad9f755c6f267e61625846afc044c93d03af372
chriskahdian <34841848+chriskahdian@users.noreply.github.com>
2020-09-21 14:28:01 -0700

    lesson 2.4

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+
+app.use(morgan("common"));
+
+app.get("/movies", (req, res) => {
+  res.json(top10);
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my movie list");
+});
+
+app.use(express.static("public"));
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("error");
+});
+
+app.listen(8080, () => console.log("app running on port 8080"));
