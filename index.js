ef0efe7cd1e75dc0a4bfdb6916337afff59142d3
Chris <chriskahdian@gmail.com>
2020-10-21 14:27:45 -0700

    2.10 - 3

--- index.js
+++ index.js
@@ -19,7 +19,11 @@ const mongoose = require("mongoose");
 const Models = require("./models.js");
 const Movies = Models.Movie;
 const Users = Models.User;
-mongoose.connect("mongodb://localhost:27017/MyFlixDB", {
+// mongoose.connect("mongodb://localhost:27017/MyFlixDB", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+mongoose.connect(process.env.CONNECTION_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
